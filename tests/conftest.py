27b9b1202fac3d89491ca05de4d91b4f0e63724f
arina <sav2405@gmail.com>
2023-02-16 16:48:00 +0400

    homework 13.2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
 from shop.shop import Item
 
+
 @pytest.fixture
 def item():
-    return Item(name='тестовый товар', price=10000, quantity=20)
+    return Item(name='товар 1', price=10000, quantity=20)
+
+
+@pytest.fixture()
+def exist_item():
+    return Item.all[0]
